58dc2eba24cc0cd88b6afa914cd10d5ae0f7478b
Jeff Cooper <jeff@cuperman.net>
2013-04-14 17:00:30 -0700

    Removed connect server from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,9 @@
 'use strict';
-var path = require('path');
-var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
-
-var folderMount = function folderMount(connect, point) {
-  return connect.static(path.resolve(point));
-};
-
 module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     livereload: {
       port: 35729 // Default livereload listening port.
-    },
-    connect: {
-      livereload: {
-        options: {
-          port: 9001,
-          middleware: function(connect, options) {
-            return [lrSnippet, folderMount(connect, options.base)];
-          }
-        }
-      }
     },
     // Configuration to be run (and then tested)
     regarde: {
@@ -33,8 +16,7 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-regarde');
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-livereload');
 
-  grunt.registerTask('default', ['livereload-start', 'connect', 'regarde']);
+  grunt.registerTask('default', ['livereload-start', 'regarde']);
 };
